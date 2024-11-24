bcec1eaed192ed3cb4d474a7d2579cd555067501
Ksenija Cvjetan <ksenija.cvjetan@gmail.com>
2024-11-24 15:42:31 +0100

    Instaliranje axiosa i router doma

--- e-dnevnik-front/src/index.js
+++ e-dnevnik-front/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//ovde se renderuje komponenta
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
